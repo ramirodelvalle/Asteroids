fccc1c2d7a17e226a8ae4c9341419db719e4c1ef
ramirodelvalle <ramiroprk@gmail.com>
2022-02-26 13:40:44 -0300

    Added the screen wrapper system

---
+++ Assets/Scripts/Systems/ScreenWrapperSystem.cs
@@ -0,0 +1,35 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+
+[AlwaysSynchronizeSystem]
+public class ScreenWrapperSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        Entities.ForEach((ref Translation translation, in PlayerData player, in LocalToWorld localToWorld) =>
+        {
+            float range = 0.5f;
+            if (translation.Value.y > 5 + range)
+            {
+                translation.Value.y = -5;
+            }
+
+            if (translation.Value.y < -5 - range)
+            {
+                translation.Value.y = 5;
+            }
+
+            if (translation.Value.x > 5 + range)
+            {
+                translation.Value.x = -5;
+            }
+
+            if (translation.Value.x < -5 - range)
+            {
+                translation.Value.x = 5;
+            }
+        }).Run();
+        return default;
+    }
+}
