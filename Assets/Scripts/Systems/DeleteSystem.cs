2921336e35a7f8ab978e1532ec8cef25cfa7ea9d
ramirodelvalle <ramiroprk@gmail.com>
2022-02-27 15:53:32 -0300

    Added trigger collision by laser and asteroid destroy

---
+++ Assets/Scripts/Systems/DeleteSystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Collections;
+
+[AlwaysSynchronizeSystem]
+[UpdateAfter(typeof(OnTriggerEnterSystem))]
+public class DeleteSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        EntityCommandBuffer commandBuffer = new EntityCommandBuffer(Allocator.TempJob);
+        //ASTEROID
+        Entities
+            .WithAll<DeleteTag>()
+            .WithStructuralChanges()
+            .ForEach((Entity entity) =>
+        {
+            GameManager.instance.IncreaseScore();
+            commandBuffer.DestroyEntity(entity);
+        }).Run();
+
+        commandBuffer.Playback(EntityManager);
+        commandBuffer.Dispose();
+    }
+}
---
+++ Assets/Scripts/Systems/OnTriggerEnterSystem.cs
@@ -0,0 +1,74 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Physics;
+using Unity.Physics.Systems;
+
+public class OnTriggerEnterSystem : JobComponentSystem
+{
+    private BuildPhysicsWorld buildPhysicsWorld;
+    private StepPhysicsWorld stepPhysicsWorld;
+
+    private EndSimulationEntityCommandBufferSystem commandBufferSystem;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
+        stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+        commandBufferSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    struct OnTriggerSystemJob : ITriggerEventsJob
+    {
+        [ReadOnly] public ComponentDataFromEntity<PlayerData> allPlayers;
+        [ReadOnly] public ComponentDataFromEntity<LaserData> allLasers;
+        [ReadOnly] public ComponentDataFromEntity<AsteroidData> allAsteroids;
+
+        public EntityCommandBuffer entityCommandBuffer;
+
+        public void Execute(TriggerEvent triggerEvent)
+        {
+            Entity entityA = triggerEvent.EntityA;
+            Entity entityB = triggerEvent.EntityB;
+            
+            if (allAsteroids.HasComponent(entityA) && allAsteroids.HasComponent(entityB))
+            {
+                UnityEngine.Debug.Log("choque entre asteroides");
+                return;
+            }
+
+            if (allLasers.HasComponent(entityA) && allAsteroids.HasComponent(entityB))
+            {
+                UnityEngine.Debug.Log("laser Entity A: " + entityA + " choco con asteroide: " + entityB);
+                entityCommandBuffer.AddComponent(entityB, new DeleteTag());
+                entityCommandBuffer.DestroyEntity(entityA);
+            }
+            else if (allAsteroids.HasComponent(entityA) && allLasers.HasComponent(entityB))
+            {
+                UnityEngine.Debug.Log("asteroide Entity A: " + entityA + " laser con laser: " + entityB);
+                entityCommandBuffer.AddComponent(entityA, new DeleteTag());
+                entityCommandBuffer.DestroyEntity(entityB);
+            }
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        var job = new OnTriggerSystemJob();
+        job.allPlayers = GetComponentDataFromEntity<PlayerData>(true); //true para indicar que es readonly
+        job.allLasers = GetComponentDataFromEntity<LaserData>(true); 
+        job.allAsteroids = GetComponentDataFromEntity<AsteroidData>(true); 
+
+        job.entityCommandBuffer = commandBufferSystem.CreateCommandBuffer();
+
+        JobHandle jobHandle = job.Schedule(stepPhysicsWorld.Simulation, ref buildPhysicsWorld.PhysicsWorld,
+            inputDependencies);
+
+        commandBufferSystem.AddJobHandleForProducer(jobHandle);
+        return jobHandle;
+    }
+}
